b514fa18851b580b589a4f022d7732e322fe5de7
Nikita Tale <talenikita3@gmail.com>
2026-02-23 19:15:44 +0530

    Update some changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Features from './components/Features'
 import Pricing from './components/Pricing'
 import About from './components/About'
 import Navbar from './Navbar'
-import Footer from './Footer'
+
 
 const AppContent = () => {
 
